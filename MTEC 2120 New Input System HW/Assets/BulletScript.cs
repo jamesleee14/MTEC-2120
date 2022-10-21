0b5ee6cbbc6f82f9fe77c9c16b44d3a2e6f2b7c0
James Lee <James.Lee@mail.citytech.cuny.edu>
2022-10-21 02:14:35 -0400

    pushing new input system hw(shooting bullet)

--- MTEC 2120 New Input System HW/Assets/BulletScript.cs
+++ MTEC 2120 New Input System HW/Assets/BulletScript.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class BulletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bullet;
+    public float projectileForce = 100f;
+    public void OnFire(InputAction.CallbackContext context)
     {
-        
+        GameObject go = Instantiate(bullet);
+        go.GetComponent<Rigidbody>().AddForce(transform.forward);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
